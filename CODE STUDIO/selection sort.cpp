a607bae9a9c66939bed56edf790fc2806724e289
priyanshu mundra <97145602+Spyder15@users.noreply.github.com>
2022-01-31 15:35:44 +0530

    Create selection sort.cpp

---
+++ CODE STUDIO/selection sort.cpp
@@ -0,0 +1,13 @@
+void selectionSort(vector<int>& arr, int n)
+{   
+    for(int i=0;i<n;i++)
+    {
+        int minIndex=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[minIndex])
+                minIndex=j;
+        }
+    swap(arr[i],arr[minIndex]);
+    }
+}
