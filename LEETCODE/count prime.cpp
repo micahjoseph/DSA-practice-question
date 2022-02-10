33c3502b306bc09501de96a439820982f9e8d712
priyanshu mundra <97145602+Spyder15@users.noreply.github.com>
2022-02-10 13:11:33 +0530

    Update count prime.cpp
    changing solution

--- LEETCODE/count prime.cpp
+++ LEETCODE/count prime.cpp
@@ -1,24 +1,33 @@
-class Solution {
-public:
-    int countPrimes(int n) {
-        int cnt=0;
-        vector<bool>prime(n++, true);
-        
-        prime[0]=prime[1]=false;
-        
-        for(int i=2; i<n; i++)
+#include<iostream>
+using namespace std;
+
+bool isPrime(int n) {
+
+    if(n<=1)
+    return false;
+
+    for(int i=2; i<n; i++)
+    {
+        if(n%i==0)
         {
-            if(prime[i])
-            {
-                cnt++;
-                
-                for(int j=2*i; j<n; j=j+i)
-                {
-                    prime[j]=0;
-                    }
-            }
+            return false;
         }
-        
-        return cnt;
     }
-};
+    return true;
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    if(isPrime(n)) {
+        cout << "It is a Prime Number" << endl;
+    }
+    else{
+        cout << " It is not a Prime Number" << endl;
+    }
+
+
+    return 0;
+}
