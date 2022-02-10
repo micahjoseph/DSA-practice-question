507c578c65aef1d8b3f51690f2a23988facc7227
priyanshu mundra <97145602+Spyder15@users.noreply.github.com>
2022-02-10 13:32:18 +0530

    Update count prime.cpp

--- LEETCODE/count prime.cpp
+++ LEETCODE/count prime.cpp
@@ -31,3 +31,36 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+
+
+// sieve of eratorsgthese mer=thod
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        int cnt=0;
+        vector<bool>prime(n+1, true);
+        
+        prime[0]=prime[1]=false;
+        
+        for(int i=2; i<n; i++)
+        {
+            if(prime[i])
+            {
+                cnt++;
+                
+                for(int j=2*i; j<n; j=j+i)
+                {
+                    prime[j]=0;
+                    }
+            }
+        }
+        
+        return cnt;
+    }
+};
